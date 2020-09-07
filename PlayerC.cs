c73442109996feec47d6ae0595e24442e0171361
jsolano813 <jsolano.813@gmail.com>
2020-09-07 17:42:12 -0400

    Changes to ReadMe

--- PlayerC.cs
+++ PlayerC.cs
@@ -1,51 +1,39 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerC : MonoBehaviour {
+public class PlayerC : MonoBehaviour
+{
     public float speed = 10.0f;
-
-    public string LTVertical;
-    public string LTHorizontal;
-
-    public string J;
-    public float jumpHeight = 8;
-    private bool isFalling = false;
+    public float jumpHeight = 5.0f;
+    private bool canJump = true;
     public Rigidbody rbody;
 
-	// Use this for initialization
-	void Start () {
+    void Start()
+    {
         rbody = GetComponent<Rigidbody>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        Movement();
-        jump();
     }
 
-    void Movement()
+    void Update()
     {
-        float translation = Input.GetAxis(LTVertical) * speed;
-        float straffe = Input.GetAxis(LTHorizontal) * speed;
+        float translation = Input.GetAxis("LSVertical") * speed;
+        float straffe = Input.GetAxis("LSHorizontal") * speed;
         translation *= Time.deltaTime;
         straffe *= Time.deltaTime;
 
         transform.Translate(straffe, 0, translation);
-    }
 
-    void OnCollisionStay(Collision collision)
-    {
-        isFalling = false;
-    }
-
-    void jump()
-    {
-        if (Input.GetButtonDown(J) && isFalling == false)
+        if (Input.GetButtonDown("Jump") && canJump)
         {
-            Debug.Log("jump initiated");
-            rbody.velocity = new Vector3(0, jumpHeight, 0);
+            rbody.AddForce(new Vector3(0, jumpHeight, 0), ForceMode.Impulse);
+            canJump = false;
         }
-        isFalling = true;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        canJump = true;
     }
 }
+
--- README.md
+++ README.md
@@ -4,4 +4,50 @@ Hi Everyone,
 
 Below I will outline how to use these plug and play scripts to automatically use a controller to control your characters in Unity from the start of a 3D Project.
 
-TEST Commit
+## Create a Unity 3D Enviornment
+
+1. Add a plane to your enviornment and set the Scale's X Y and Z (under the Transform tab in the inspector) to 10, this will act as the floor.
+2. Add a cube slightly above your plane, this will act as your character. Also add a Ridgid Body to it.
+3. Create two new materials in your assets folder, and change the Albedo to which ever color you'd like, one for your character and one for the floor. When you are finished, drag these materials on top of the objects to change their color.
+
+## Setup Input Axis
+
+Import the Gaming Scripts:
+Go to the folder you've cloned and drag the assets in.
+
+Add three new Axes for the Inputs of the Left Thumbstick and A button.
+
+Go to **Edit --> Project Settings --> Input**, Open the Axis Tab and set the Size to 3 higher than it already is.
+If the Size of Axis is 18, set it to 21.
+
+These are the settings for the Axes
+Name: Jump
+Positive Button: joystick button 0
+Gravity: 1000
+Dead Zone: 0
+Sensitivity: 1000
+Type: Key or Mouse Button
+Axis: X axis
+Joy Num: Get Motion from all Joysticks
+
+Name: LSHorizontal
+Gravity: 0
+Dead Zone: .02
+Sensitivity: 1
+Type: Joystick Axis
+Axis: X axis
+Joy Num: Get Motion from all Joysticks
+
+Name: LSVertical
+Gravity: 0
+Dead Zone: .02
+Sensitivity: 1
+Type: Joystick Axis
+Axis: Y axis
+Joy Num: Get Motion from all Joysticks
+
+![](ReadMeAssets/AxesImage.png)
+
+Your Character will now be able to move and jump using the Left Thumbstick and the A button on your Xbox controller.
+
+
